d2b7d152360efe4f05122fdf509a62523dc9ef1e
Joseph Salinas <140462648+Joseph-0000@users.noreply.github.com>
2025-02-18 05:31:28 +0800

    crud logic

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./routes/Product.js"
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Server is running");
-})
+app.use("/api/products", Product);
 
 app.listen(5000, () => {
     connectDB();
     console.log("Server is running on port 5000");
 })
+
 
