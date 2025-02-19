98622fcaf56c7b640fa6752b52426a02db4a0d5d
Joseph-0000 <josephsalinas555@gmail.com>
2025-02-19 15:52:08 +0800

    frontend update

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+
+const HomePage = () => {
+  return (
+    <>
+    <h1>HomePage</h1>
+  </>
+  )
+}
+
+export default HomePage
